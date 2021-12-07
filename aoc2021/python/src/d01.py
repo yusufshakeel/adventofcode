8a3db2f6e1e3023aa760cf8b0a9223c6077d7823
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-12-08 00:06:42 +0530

    python code for day 1

--- aoc2021/python/src/d01.py
+++ aoc2021/python/src/d01.py
@@ -1,5 +1,22 @@
+def sum(input):
+  num = input[0]
+  count = 0
+  for i in input[1:]:
+    if i > num:
+      count += 1
+      num = i
+    else:
+      num = i
+  return count
+
+def trio(input):
+  enrichedInput = []
+  for i in range(len(input)-2):
+    enrichedInput.append(input[i]+input[i+1]+input[i+2])
+  return enrichedInput
+
 def part1(input):
-  return input
+  return sum(input)
 
 def part2(input):
-  return input
+  return sum(trio(input))
--- aoc2021/python/tests/d01_test.py
+++ aoc2021/python/tests/d01_test.py
@@ -1,7 +1,19 @@
+import os
 from src.d01 import part1, part2
 
+curr_dir = os.path.dirname(__file__)
+sample = []
+for line in open(curr_dir + '/../data/01-sample.txt'):
+  sample.append(int(line.strip()))
+
+input = []
+for line in open(curr_dir + '/../data/01-input.txt'):
+  input.append(int(line.strip()))
+
 def test_part1():
-  assert part1({"a":1,"b":2}) == { "a": 1, "b": 2 }, "Failed 01-01"
+  assert part1(sample) == 7
+  assert part1(input) == 1184
 
 def test_part2():
-  assert part2(1) == 1, "Failed 01-02"
+  assert part2(sample) == 5
+  assert part2(input) == 1158
